5beb6a1bbd62de184420ed2970f1cdc418d7eb0c
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-10 15:38:07 -0700

    finished MVP, styled elements

--- src/App.js
+++ src/App.js
@@ -23,9 +23,13 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
-        <Specials />
-        <Numbers />
-        <Operators />
+        <div className="button-container">
+          <div className="specials-numbers">
+            <Specials />
+            <Numbers />
+          </div>
+          <Operators />
+        </div>
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,7 +10,7 @@ export const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
   return (
-    <div>
+    <div className="numbers-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
