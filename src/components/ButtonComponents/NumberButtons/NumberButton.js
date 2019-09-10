443e798ba3403a0cb9f1884a20726e1a11b47621
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-10 14:05:29 -0700

    npm install, implemented numbers and numberbutton, exported data

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = ({ number }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className="number-button">{number}</button>
     </>
   );
 };
