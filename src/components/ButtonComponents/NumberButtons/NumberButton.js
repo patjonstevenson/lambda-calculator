be1877aa8fdb68a1a51e1e50620c74515274698b
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-10 22:07:03 -0700

    changed NumberButton to remove leading zero

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,10 @@ export const NumberButton = ({ number, total, updater }) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="number-button" onClick={() => updater(total + number)}>
+      <button
+        className="number-button"
+        onClick={() => updater(Number(total + number).toString())}
+      >
         {number}
       </button>
     </>
